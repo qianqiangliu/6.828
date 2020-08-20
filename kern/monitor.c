dd799f3f45092886dd8b5b38600b21295b8377b6
Qianqiang Liu <qianqiang.liu@linkplay.com>
2020-08-21 01:11:18 +0800

    my solution for lab1 exercise 11

--- kern/monitor.c
+++ kern/monitor.c
@@ -58,6 +58,14 @@ int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
 	// Your code here.
+	uint32_t *ebp = (uint32_t *)read_ebp();
+
+	cprintf("Stack backtrace:\n");
+	while (ebp) {
+		cprintf("  ebp %08x  eip %08x  args %08x %08x %08x %08x %08x\n",
+			ebp, ebp[1], ebp[2], ebp[3], ebp[4], ebp[5], ebp[6]);
+		ebp = (uint32_t *)*ebp;
+	}
 	return 0;
 }
 
