07947664ca8bc4983796dff9584bd8e24dd8430f
Qianqiang Liu <qianqiang.liu@linkplay.com>
2020-08-27 19:49:02 +0800

    my solution for lab4 exercise 1

--- kern/pmap.c
+++ kern/pmap.c
@@ -582,7 +582,14 @@ mmio_map_region(physaddr_t pa, size_t size)
 	// Hint: The staff solution uses boot_map_region.
 	//
 	// Your code here:
-	panic("mmio_map_region not implemented");
+	size = ROUNDUP(pa + size, PGSIZE);
+	pa = ROUNDDOWN(pa, PGSIZE);
+	size -= pa;
+	if (base + size >= MMIOLIM)
+		panic("mmio_map_region overflows MMIOLIM\n");
+	boot_map_region(kern_pgdir, base, size, pa, PTE_PCD | PTE_PWT | PTE_W);
+	base += size;
+	return (void *)(base - size);
 }
 
 static uintptr_t user_mem_check_addr;
