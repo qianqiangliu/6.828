5d72c3ab425d4c1e2654ae3784ffd0a1ccdf8c95
Qianqiang Liu <qianqiang.liu@linkplay.com>
2020-08-28 18:26:12 +0800

    my solution for lab4 exercise 8

--- kern/syscall.c
+++ kern/syscall.c
@@ -138,7 +138,14 @@ static int
 sys_env_set_pgfault_upcall(envid_t envid, void *func)
 {
 	// LAB 4: Your code here.
-	panic("sys_env_set_pgfault_upcall not implemented");
+	struct Env *env;
+	int ret;
+
+	ret = envid2env(envid, &env, 1);
+	if (ret < 0)
+		return ret;
+	env->env_pgfault_upcall = func;
+	return 0;
 }
 
 // Allocate a page of memory and map it at 'va' with permission
